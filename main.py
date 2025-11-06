f4fcd8def5462423cfc0663b66fbf15ad149e390
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2025-11-06 20:08:47 +0700

    adding essentials file

---
+++ main.py
@@ -0,0 +1,32 @@
+import math
+import pygame
+import pymunk
+import pymunk.pygame_util
+
+# Inisialisasi pygame
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Billiard Game")
+
+def main(window):
+    done = False
+    clock = pygame.time.Clock()
+    fps = 60
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Warna background
+        window.fill((0, 0, 0))
+
+        # Update tampilan
+        pygame.display.flip()
+        clock.tick(fps)
+
+    pygame.quit()
+
+# main program
+main(window)
