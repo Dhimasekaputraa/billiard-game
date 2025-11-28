62daa53e0ed49de4219bfe8b36682bea06ad4071
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2025-11-28 08:29:07 +0700

    added gamemode selection and pool rules for multiplayer

--- gamestate.py
+++ gamestate.py
@@ -21,13 +21,20 @@ class Player(Enum):
 
 
 class GameState:
-    def __init__(self):
+    def __init__(self, game_mode="single"):
+        self.game_mode = game_mode
         pygame.init()
         pygame.display.set_caption(config.window_caption)
         event.set_allowed_events()
         zope.event.subscribers.append(self.game_event_handler)
         self.canvas = graphics.Canvas()
         self.fps_clock = pygame.time.Clock()
+        
+        # Inisialisasi variabel penting agar event handler tidak error
+        self.white_ball_1st_hit_is_set = False
+        self.potted = []
+        self.balls = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.OrderedUpdates()
 
     def fps(self):
         return self.fps_clock.get_fps()
@@ -49,7 +56,9 @@ class GameState:
 
     def game_event_handler(self, event):
         if event.type == "POTTED":
-            self.table_coloring.update(self)
+            # Tambahkan pengecekan apakah table_coloring sudah ada
+            if hasattr(self, 'table_coloring'):
+                self.table_coloring.update(self)
             self.balls.remove(event.data)
             self.all_sprites.remove(event.data)
             self.potted.append(event.data.number)
@@ -125,15 +134,10 @@ class GameState:
 
     def generate_table(self):
         table_side_points = np.empty((1, 2))
-        # holes_x and holes_y holds the possible xs and ys of the table holes
-        # with a position ID in the second tuple field
-        # so the top left hole has id 1,1
         holes_x = [(config.table_margin, 1), (config.resolution[0] /
                                               2, 2), (config.resolution[0] - config.table_margin, 3)]
         holes_y = [(config.table_margin, 1),
                    (config.resolution[1] - config.table_margin, 2)]
-        # next three lines are a hack to make and arrange the hole coordinates
-        # in the correct sequence
         all_hole_positions = np.array(
             list(itertools.product(holes_y, holes_x)))
         all_hole_positions = np.fliplr(all_hole_positions)
@@ -142,8 +146,6 @@ class GameState:
         for hole_pos in all_hole_positions:
             self.holes.add(table_sprites.Hole(hole_pos[0][0], hole_pos[1][0]))
             if hole_pos[0][1] == 2:
-                # hole_pos[0,1]=2 means x coordinate ID is 2 which means this
-                # hole is in the middle
                 offset = config.middle_hole_offset
             else:
                 offset = config.side_hole_offset
@@ -153,10 +155,8 @@ class GameState:
                 offset = np.flipud(offset) * [-1, 1]
             table_side_points = np.append(
                 table_side_points, [hole_pos[0][0], hole_pos[1][0]] + offset, axis=0)
-        # deletes the 1st point in array (leftover form np.empty)
         table_side_points = np.delete(table_side_points, 0, 0)
         for num, point in enumerate(table_side_points[:-1]):
-            # this will skip lines inside the circle
             if num % 4 != 1:
                 self.table_sides.append(table_sprites.TableSide(
                     [point, table_side_points[num + 1]]))
@@ -170,14 +170,23 @@ class GameState:
 
     
     def check_potted(self):
-        self.can_move_white_ball = False  # if white ball is potted, it will be created again and placed in the middle
+        self.can_move_white_ball = False
         if 0 in self.potted:
             self.create_white_ball()
             self.cue.target_ball = self.white_ball
             self.potted.remove(0)
+            # Hanya panggil turn_over di multiplayer mode
+            if self.game_mode == "multiplayer":
+                self.turn_over(True)
+        if 8 in self.potted:
+            # Hanya ada game over di multiplayer mode
+            if self.game_mode == "multiplayer":
+                if self.potting_8ball[self.current_player]:
+                    self.game_over(self.current_player == Player.Player1)
+                else:
+                    self.game_over(self.current_player != Player.Player1)
 
     def check_remaining(self):
-        # a check if all striped or solid balls were potted
         stripes_remaining = False
         solids_remaining = False
         for remaining_ball in self.balls:
@@ -186,7 +195,6 @@ class GameState:
                 solids_remaining = solids_remaining or not remaining_ball.ball_type == BallType.Striped
         ball_type_remaining = {BallType.Solid: solids_remaining, BallType.Striped: stripes_remaining}
 
-        # decides if on of the players (or both) should be potting 8ball
         self.potting_8ball = {Player.Player1: not ball_type_remaining[self.ball_assignment[Player.Player1]],
                               Player.Player2: not ball_type_remaining[self.ball_assignment[Player.Player2]]}
 
@@ -200,3 +208,72 @@ class GameState:
 
     def check_pool_rules(self):
         self.check_potted()
+
+        if self.game_mode == "multiplayer":
+            if self.ball_assignment is not None:
+                self.check_remaining()
+            self.first_hit_rule()
+            self.potted_ball_rules()
+            self.on_next_hit()
+    
+    def on_next_hit(self):
+        self.white_ball_1st_hit_is_set = False
+        self.turn_ended = False
+        self.potted = []
+
+    def potted_ball_rules(self):
+        if len(self.potted) > 0:
+            potted_stripe_count = len([x for x in self.potted if x > 8])
+            potted_solid_count = len([x for x in self.potted if x < 8])
+            only_stripes_potted = potted_solid_count == 0 and potted_stripe_count > 0
+            only_solids_potted = potted_stripe_count == 0 and potted_solid_count > 0
+
+            if only_solids_potted or only_stripes_potted:
+                selected_ball_type = BallType.Striped if only_stripes_potted else BallType.Solid
+                if self.ball_assignment is None:
+                    other_player, = set(Player) - {self.current_player}
+                    other_ball_type, = set(BallType) - {selected_ball_type}
+                    self.ball_assignment = {self.current_player: selected_ball_type, other_player: other_ball_type}
+                    self.potting_8ball = {self.current_player: False, other_player: False}
+                elif self.ball_assignment[self.current_player] != selected_ball_type:
+                    self.turn_over(False)
+        else:
+            self.turn_over(False)
+
+    def first_hit_rule(self):
+        if not self.white_ball_1st_hit_is_set:
+            self.turn_over(True)
+        elif self.ball_assignment is not None:
+            if not self.white_ball_1st_hit_8ball and self.ball_assignment[
+                self.current_player] != self.white_ball_1st_hit_type:
+                self.turn_over(True)
+            elif self.white_ball_1st_hit_8ball:
+                self.turn_over(not self.potting_8ball[self.current_player])
+
+    def game_over(self, p1_won):
+        font = config.get_default_font(config.game_over_label_font_size)
+        if p1_won:
+            text = "PLAYER 1 WON!"
+        else:
+            text = "PLAYER 2 WON!"
+        rendered_text = font.render(text, False, (255, 255, 255))
+        self.canvas.surface.blit(rendered_text, (config.resolution - font.size(text)) / 2)
+        pygame.display.flip()
+        pygame.event.clear()
+        paused = True
+        while paused:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                paused = False
+        self.is_game_over = True
+
+    def turn_over(self, penalize):
+        if not self.turn_ended:
+            self.turn_ended = True
+            self.turn_number += 1
+            if self.current_player == Player.Player1:
+                self.current_player = Player.Player2
+            else:
+                self.current_player = Player.Player1
+        if penalize:
+            self.can_move_white_ball = True
